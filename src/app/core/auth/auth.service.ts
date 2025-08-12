fa164e65bf2c84de21ba5298c72a6ddd433b0699
Truenopay123 <bryanaxelcortescortes@gmail.com>
2025-08-12 15:28:39 -0600

    Despliegue

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -38,7 +38,7 @@ class LoggerService {
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = `${environment.apiUrl}/auth`;
+  private apiUrl = `${environment.apiUrlAuth}/auth`;
 
   constructor(
     private http: HttpClient,
--- src/app/core/environments/environment.prod.ts
+++ src/app/core/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api-gateway-aft7.onrender.com',
   apiUrlTask: 'https://task-services-vrvc.onrender.com',
+  apiUrlAuth: 'https://auth-services-ja81.onrender.com'
 
 };
